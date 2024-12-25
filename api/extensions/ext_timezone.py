60151b51f61ea24e134b049551b72e6b29636d12
twonian <nianzu.xu@trinapower.com>
2024-12-25 23:26:31 +0800

    修改时区

--- api/extensions/ext_timezone.py
+++ api/extensions/ext_timezone.py
@@ -5,7 +5,7 @@ from dify_app import DifyApp
 
 
 def init_app(app: DifyApp):
-    os.environ["TZ"] = "UTC"
+    os.environ["TZ"] = "Asia/Shanghai"
     # windows platform not support tzset
     if hasattr(time, "tzset"):
         time.tzset()
